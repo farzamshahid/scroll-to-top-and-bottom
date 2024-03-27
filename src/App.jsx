6f4234103f892007796480512c7fd1361410b0e1
farzamshahid <farzamshahid6@gmail.com>
2024-03-27 15:08:43 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ScrollToSection from './scroll-section.jsx'
+import ScrollToTopAndBottom from './scroll-from-top-to-bottom.jsx'
+
+function App() {
+
+  return (
+    <>
+      <ScrollToTopAndBottom />
+      <ScrollToSection />
+    </>
+  )
+}
+
+export default App
